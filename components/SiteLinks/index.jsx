0e70d2bb759e86d2d041f8f02a76d7b98a58577a
benjongbloedt <bjongbloedt@hotmail.com>
2017-02-10 13:32:50 -0600

    initial move to gatsby

---
+++ components/SiteLinks/index.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { RouteHandler, Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import { config } from 'config'
+import './style.css'
+import '../../static/fonts/fontawesome/style.css'
+
+class SiteLinks extends React.Component {
+    render () {
+
+      return (
+        <div className="blog-links">
+          <ul>
+            {config.siteTwitterUrl && (
+              <li>
+                <a href={config.siteTwitterUrl}>
+                  <i className="fa fa-twitter" />
+                </a>
+              </li>
+            )}
+            {config.siteLinkedinUrl && (
+              <li>
+                <a href={config.siteLinkedinUrl}>
+                  <i className="fa fa-linkedin" />
+                </a>
+              </li>
+            )}
+            {config.siteStackOverflowUrl && (
+              <li>
+                <a href={config.siteStackOverflowUrl}>
+                  <i className="fa fa-stack-overflow" />
+                </a>
+              </li>
+            )}
+            {config.siteGithubUrl && (
+              <li>
+                <a href={config.siteGithubUrl}>
+                  <i className="fa fa-github-alt" />
+                </a>
+              </li>
+            )}
+            {config.siteVkUrl && (
+              <li>
+                <a href={config.siteVkUrl}><i className="fa fa-vk" /></a>
+              </li>
+            )}
+          </ul>
+          <ul>
+            {config.siteEmailUrl && (
+              <li>
+                <a href={config.siteEmailUrl}>
+                  <i className="fa fa-envelope-o" />
+                </a>
+              </li>
+            )}
+            {config.siteTelegramUrl && (
+              <li>
+                <a href={config.siteTelegramUrl}>
+                  <i className="fa fa-paper-plane" />
+                </a>
+              </li>
+                )}
+          </ul>
+          <ul>
+            {config.siteRssUrl && (
+              <li>
+                <a href={config.siteRssUrl}><i className="fa fa-rss" /></a>
+              </li>
+            )}
+          </ul>
+        </div>
+      )
+  }
+}
+
+export default SiteLinks
