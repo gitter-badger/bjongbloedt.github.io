4789d112d339d797f717926a6234457092181b4c
benjongbloedt <ben.jongbloedt@ave81.com>
2017-02-14 13:23:12 -0600

    adding prod check

--- html.js
+++ html.js
@@ -14,7 +14,10 @@ module.exports = React.createClass({
         const {title} = Helmet.rewind()
         const font = <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
         const favicon = <link rel="icon" type="image/png" href={require('./pages/favicon-16x16.png')} />
-        let css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
+        let css 
+        if (process.env.NODE_ENV === 'production') {
+            css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
+        } 
 
         return (
             <html lang="en">
